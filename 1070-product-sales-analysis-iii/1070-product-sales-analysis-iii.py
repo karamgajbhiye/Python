7b7a811918209c0dce2bb792117bef470008556b
Karam G <karamgajbhiye@gmail.com>
2026-02-03 20:29:48 +0530

    Time: 300 ms (79.72%), Space: 69.2 MB (86.89%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame) -> pd.DataFrame:
+    sales['first_year'] = sales.groupby('product_id')['year'].transform('min')
+    sales = sales[sales['first_year'] == sales['year']]
+    return sales[['product_id','first_year','quantity','price']]
